385a64196009f91ec7abdfcd7cbf7422c9780e25
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:19:42 +0300

    main.c

--- main.c
+++ main.c
@@ -17,42 +17,56 @@ globals_t global;
  */
 int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-	size_t numbytes = 0;
-	int bytesr = 0;
-	unsigned int con = 1;
+    if (argc != 2)
+    {
+        fputs("USAGE: monty file\n", stderr);
+        return EXIT_FAILURE;
+    }
 
-	global.flag = 1;
-	global.line = NULL;
-	if (argc != 2)
-	{
-		fputs("USAGE: monty file\n", stderr);
-		exit(EXIT_FAILURE);
-	}
-	global.fil = fopen(argv[1], "r");
-	if (global.fil == NULL)
-	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while ((bytesr = getline(&global.line, &numbytes, global.fil)) != EOF)
-	{
-		delete_jump(global.line);
-		if (global.line[0] != 35)
-		{
-			global.token = strtok(global.line, " \t\n");
-			global.opco = global.token;
-			if (global.opco != NULL)
-			{
-				global.token = strtok(NULL, " \t\n");
-				func(global.opco)(&stack, con);
-			}
-			con++;
-		}
-	}
-	free_l(&stack);
-	free(global.line);
-	fclose(global.fil);
+    global.fil = fopen(argv[1], "r");
+    if (global.fil == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-	return (0);
+    global.line = NULL;
+    size_t numbytes = 0;
+    ssize_t bytesr;
+    unsigned int con = 1;
+    stack_t *stack = NULL;
+
+    while ((bytesr = getline(&global.line, &numbytes, global.fil)) != -1)
+    {
+        delete_jump(global.line);
+
+        if (global.line[0] != '#')
+        {
+            global.token = strtok(global.line, " \t\n");
+            global.opco = global.token;
+
+            if (global.opco != NULL)
+            {
+                global.token = strtok(NULL, " \t\n");
+                void (*op_func)(stack_t **, unsigned int) = func(global.opco);
+                if (op_func != NULL)
+                    op_func(&stack, con);
+                else
+                {
+                    fprintf(stderr, "L%u: unknown instruction %s\n", con, global.opco);
+                    free(global.line);
+                    fclose(global.fil);
+                    free_l(&stack);
+                    return EXIT_FAILURE;
+                }
+            }
+            con++;
+        }
+    }
+
+    free_l(&stack);
+    free(global.line);
+    fclose(global.fil);
+
+    return EXIT_SUCCESS;
 }
