e014f1faefe3d841bbd95c43daac2685b333f4b7
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:12:01 +0300

    find_opcode.c

--- find_opcode.c
+++ find_opcode.c
@@ -6,39 +6,47 @@
  * Return: A function pointer.
  */
 
+#include "monty.h"
+
+/**
+ * func - Takes arguments and returns a function pointer.
+ * @tokens: arguments
+ * Return: A function pointer.
+ */
+
 void (*func(char *tokens))(stack_t **stack, unsigned int num_line)
 {
-    static const struct {
-        const char *opcode;
-        void (*func)(stack_t **stack, unsigned int num_line);
-    } ops[] = {
-        {"push", _push},
-        {"pall", _pall},
-        {"pint", _pint},
-        {"pop", _pop},
-        {"swap", _swap},
-        {"add", _add},
-        {"nop", _nop},
-        {"sub", _sub},
-        {"div", _div},
-        {"mul", _mul},
-        {"mod", _mod},
-        {"pchar", _pchar},
-        {"pstr", _pstr},
-        {"rotl", _rotl},
-        {"rotr", _rotr},
-        {"stack", _stack},
-        {"queue", _queue},
-        {"err", _error},
-        {"#", _nop},
-        {NULL, NULL}
-    };
+instruction_t ops[] = {
+	{"push", _push},
+	{"pall", _pall},
+	{"pint", _pint},
+	{"pop", _pop},
+	{"swap", _swap},
+	{"add", _add},
+	{"nop", _nop},
+	{"sub", _sub},
+	{"div", _div},
+	{"mul", _mul},
+	{"mod", _mod},
+	{"pchar", _pchar},
+	{"pstr", _pstr},
+	{"rotl", _rotl},
+	{"rotr", _rotr},
+	{"stack", _stack},
+	{"queue", _queue},
+	{"err", _error},
+	{"#", _nop},
+	{NULL, NULL}};
+int i = 0;
+int opc;
 
-    for (int i = 0; ops[i].opcode != NULL; i++)
-    {
-        if (strcmp(ops[i].opcode, tokens) == 0)
-            return ops[i].func;
-    }
+while (i < 19)
+{
+	opc = strcmp(ops[i].opcode, tokens);
+	if (opc == 0)
+		return (ops[i].f);
+	i++;
+}
 
-    return ops[17].func;
+return (ops[17].f);
 }
