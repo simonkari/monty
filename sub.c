74ecabf93bd8b2d8c72d95eb2896004276a2c270
simonkari <kariithisimon1715@gmail.com>
2023-05-25 00:13:40 +0300

    sub.c

--- sub.c
+++ sub.c
@@ -7,29 +7,29 @@
  * @num_line: number line
  */
 
-sub(stack_t **stack, unsigned int num_line)
+void _sub(stack_t **stack, unsigned int num_line)
 {
-stack_t *temp1;
-stack_t *temp2;
-int rest = 0;
+	stack_t *temp1;
+	stack_t *temp2;
+	int rest = 0;
 
-if (*stack && (*stack)->next)
-{
-temp2 = (*stack)->next;
-rest = temp2->n - (*stack)->n;
-temp1 = *stack;
-*stack = (*stack)->next;
-if (*stack)
-(*stack)->prev = NULL;
-free(temp1);
-(*stack)->n = rest;
-}
-else
-{
-dprintf(2, "L%u: can't sub, stack too short\n", num_line);
-free(global.line);
-fclose(global.fil);
-free_l(stack);
-exit(EXIT_FAILURE);
-}
+	if (*stack && (*stack)->next)
+	{
+		temp2 = (*stack)->next;
+		rest = temp2->n - (*stack)->n;
+		temp1 = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(temp1);
+		(*stack)->n = rest;
+	}
+	else
+	{
+		dprintf(2, "L%u: can't sub, stack too short\n", num_line);
+		free(global.line);
+		fclose(global.fil);
+		free_l(stack);
+		exit(EXIT_FAILURE);
+	}
 }
