b4de1ef8c9a4b8d26337f3b989d7680cbce404d5
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:10:49 +0300

    find_opcode.c

--- find_opcode.c
+++ find_opcode.c
@@ -8,37 +8,37 @@
 
 void (*func(char *tokens))(stack_t **stack, unsigned int num_line)
 {
-instruction_t ops[] = {
-	{"push", _push},
-	{"pall", _pall},
-	{"pint", _pint},
-	{"pop", _pop},
-	{"swap", _swap},
-	{"add", _add},
-	{"nop", _nop},
-	{"sub", _sub},
-	{"div", _div},
-	{"mul", _mul},
-	{"mod", _mod},
-	{"pchar", _pchar},
-	{"pstr", _pstr},
-	{"rotl", _rotl},
-	{"rotr", _rotr},
-	{"stack", _stack},
-	{"queue", _queue},
-	{"err", _error},
-	{"#", _nop},
-	{NULL, NULL}};
-int i = 0;
-int opc;
+    static const struct {
+        const char *opcode;
+        void (*func)(stack_t **stack, unsigned int num_line);
+    } ops[] = {
+        {"push", _push},
+        {"pall", _pall},
+        {"pint", _pint},
+        {"pop", _pop},
+        {"swap", _swap},
+        {"add", _add},
+        {"nop", _nop},
+        {"sub", _sub},
+        {"div", _div},
+        {"mul", _mul},
+        {"mod", _mod},
+        {"pchar", _pchar},
+        {"pstr", _pstr},
+        {"rotl", _rotl},
+        {"rotr", _rotr},
+        {"stack", _stack},
+        {"queue", _queue},
+        {"err", _error},
+        {"#", _nop},
+        {NULL, NULL}
+    };
 
-while (i < 19)
-{
-	opc = strcmp(ops[i].opcode, tokens);
-	if (opc == 0)
-		return (ops[i].f);
-	i++;
-}
+    for (int i = 0; ops[i].opcode != NULL; i++)
+    {
+        if (strcmp(ops[i].opcode, tokens) == 0)
+            return ops[i].func;
+    }
 
-return (ops[17].f);
+    return ops[17].func;
 }
