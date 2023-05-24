226dab21cc936a56818d5b308e9d684e3dc80902
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:28:18 +0300

    nop.c

--- nop.c
+++ nop.c
@@ -5,9 +5,15 @@
  * @stack: head
  * @num_line: number of the line
  */
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything.
+ * @stack: head
+ * @num_line: number of the line
+ */
 void _nop(stack_t **stack, unsigned int num_line)
 {
-    // Do nothing
     (void)stack;
     (void)num_line;
 }
