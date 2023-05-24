5f525f9a48500484a383748ce4cf0898c3467eb2
simonkari <kariithisimon1715@gmail.com>
2023-05-25 00:26:48 +0300

    add.c

--- add.c
+++ add.c
@@ -8,23 +8,28 @@
 
 void _add(stack_t **stack, unsigned int num_line)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
+	stack_t *temp1;
+	stack_t *temp2;
+	int sum = 0;
+
+	if (*stack && (*stack)->next)
+	{
+		temp2 = (*stack)->next;
+		sum = (*stack)->n + temp2->n;
+		temp1 = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(temp1);
+		(*stack)->n = sum;
+	}
+	else
 	{
 		dprintf(2, "L%d: can't add, stack too short\n", num_line);
 		free(global.line);
 		fclose(global.fil);
 		free_l(stack);
+
 		exit(EXIT_FAILURE);
 	}
-
-	stack_t *temp1 = *stack;
-	stack_t *temp2 = (*stack)->next;
-	int sum = temp1->n + temp2->n;
-
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-
-	free(temp1);
-	(*stack)->n = sum;
 }
-
