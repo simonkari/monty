6ba11b840ebefc4cfa5c6904b6a8dc6b2742a6d6
simonkari <kariithisimon1715@gmail.com>
2023-05-25 00:15:39 +0300

    pint.c

--- pint.c
+++ pint.c
@@ -1,17 +1,23 @@
 #include "monty.h"
+
 /**
- * _pint - prints the stack
+ * _pint - prints the value at the top of the stack
  * @stack: head
  * @num_line: num line
  */
-void _pint(stack_t *stack, unsigned int num_line)
+
+void _pint(stack_t **stack, unsigned int num_line)
 {
-    if (stack == NULL)
-        return;
-    if (stack->next == NULL)
-    {
-        printf("%d\n", stack->n);
-        return;
-    }
-    _pint(stack->next, num_line);
+stack_t *temp;
+
+temp = *stack;
+if (temp == NULL)
+{
+free(global.line);
+fclose(global.fil);
+free_l(stack);
+dprintf(2, "L%u: can't pint, stack empty\n", num_line);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", temp->n);
 }
