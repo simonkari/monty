b4dea574ab984e87afae4c7d0f00e1010515a7c0
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:08:36 +0300

    div.c

--- div.c
+++ div.c
@@ -8,36 +8,26 @@
 
 void _div(stack_t **stack, unsigned int num_line)
 {
-stack_t *temp1;
-stack_t *temp2;
-int divi = 0;
-
-if (*stack && (*stack)->next)
-{
-if ((*stack)->n == 0)
-{
-dprintf(2, "L%u: division by zero\n", num_line);
-free(global.line);
-fclose(global.fil);
-free_l(stack);
-exit(EXIT_FAILURE);
-}
-temp2 = (*stack)->next;
-divi = temp2->n / (*stack)->n;
-temp1 = *stack;
-*stack = (*stack)->next;
-if (*stack)
-(*stack)->prev = NULL;
-free(temp1);
-(*stack)->n = divi;
-}
-else
-{
-dprintf(2, "L%u: can't div, stack too short\n", num_line);
-free(global.line);
-fclose(global.fil);
-free_l(stack);
-
-exit(EXIT_FAILURE);
-}
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        _error(stack, num_line);
+    }
+    else if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%u: division by zero\n", num_line);
+        free(global.line);
+        fclose(global.fil);
+        free_l(stack);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        stack_t *temp1 = *stack;
+        stack_t *temp2 = temp1->next;
+        int divi = temp2->n / temp1->n;
+        
+        temp2->n = divi;
+        *stack = temp2;
+        free(temp1);
+    }
 }
