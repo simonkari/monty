fd9db633a29de26bd95aac4d12f0eb35f4b6d8b6
simonkari <kariithisimon1715@gmail.com>
2023-05-25 01:30:44 +0300

    pop.c

--- pop.c
+++ pop.c
@@ -6,23 +6,18 @@
  */
 void _pop(stack_t **stack, unsigned int num_line)
 {
-	stack_t *temp;
+    if (*stack == NULL)
+    {
+        dprintf(2, "L%d: can't pop an empty stack\n", num_line);
+        free(global.line);
+        fclose(global.fil);
+        free_l(stack);
+        exit(EXIT_FAILURE);
+    }
 
-	temp = *stack;
-	if (temp == NULL)
-	{
-		dprintf(2, "L%d: can't pop an empty stack\n", num_line);
-		free(global.line);
-		fclose(global.fil);
-		free_l(stack);
-		exit(EXIT_FAILURE);
-	}
-	if ((*stack)->next)
-	{
-		*stack = temp->next;
-		(*stack)->prev = NULL;
-		free(temp);
-	}
-	else
-		free_l(stack);
+    stack_t *temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack != NULL)
+        (*stack)->prev = NULL;
+    free(temp);
 }
