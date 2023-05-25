56b68cde8d9bdd0668cd17870e993c1ad731f68f
simonkari <kariithisimon1715@gmail.com>
2023-05-25 19:45:18 +0300

    pall.c

--- pall.c
+++ pall.c
@@ -1,21 +1,19 @@
 #include "monty.h"
 
 /**
- * _pall - prints all the values on the stack,
- * starting from the top of the stack
- * @stack: head
- * @num_line: num linea
+ * _pall - This function prints all the values
+ * on stack, starting from the top of the stack.
+ * @stack: Represents the head of the stack.
+ * @num_line: An integer value indicating the line number.
  */
 
 void _pall(stack_t **stack, unsigned int num_line)
 {
-stack_t *temp;
+    stack_t *temp = *stack;
 
-temp = *stack;
-if (num_line)
-while (temp)
-{
-printf("%d\n", temp->n);
-temp = temp->next;
-}
+    while (temp)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
 }
