e7bf367a14bc20a846acd21520b1dc35bcd02110
simonkari <kariithisimon1715@gmail.com>
2023-05-25 19:35:34 +0300

    pint.c

--- pint.c
+++ pint.c
@@ -2,8 +2,8 @@
 
 /**
  * _pint - prints the value at the top of the stack
- * @stack: head
- * @num_line: num line
+ * @stack: Represents the head of the stack.
+ * @num_line: integer value indicating the line number
  */
 
 void _pint(stack_t **stack, unsigned int num_line)
@@ -17,6 +17,7 @@ free(global.line);
 fclose(global.fil);
 free_l(stack);
 dprintf(2, "L%u: can't pint, stack empty\n", num_line);
+
 exit(EXIT_FAILURE);
 }
 printf("%d\n", temp->n);
