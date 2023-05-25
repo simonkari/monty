1a8397451237ca7a14993ced542f01bbf26df458
simonkari <kariithisimon1715@gmail.com>
2023-05-25 19:18:09 +0300

    add.c

--- add.c
+++ add.c
@@ -8,37 +8,7 @@
 
 void _add(stack_t **stack, unsigned int num_line)
 {
-    if (*stack && (*stack)->next)
-    {
-        stack_t *temp1 = *stack;
-        stack_t *temp2 = (*stack)->next;
-        int sum = temp1->n + temp2->n;
-
-        *stack = temp2->next;
-        if (*stack)
-            (*stack)->prev = NULL;
-
-        free(temp1);
-        free(temp2);
-
-        stack_t *new_node = malloc(sizeof(stack_t));
-        if (!new_node)
-        {
-            fprintf(stderr, "Error: malloc failed\n");
-            free_l(stack);
-            exit(EXIT_FAILURE);
-        }
-
-        new_node->n = sum;
-        new_node->prev = NULL;
-        if (*stack)
-        {
-            new_node->next = *stack;
-            (*stack)->prev = new_node;
-        }
-        *stack = new_node;
-    }
-    else
+    if (!(*stack) || !(*stack)->next)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", num_line);
         free(global.line);
@@ -46,4 +16,15 @@ void _add(stack_t **stack, unsigned int num_line)
         free_l(stack);
         exit(EXIT_FAILURE);
     }
+
+    stack_t *temp1 = *stack;
+    stack_t *temp2 = (*stack)->next;
+
+    int sum = temp1->n + temp2->n;
+    temp2->n = sum;
+
+    *stack = temp2;
+    temp2->prev = NULL;
+
+    free(temp1);
 }
